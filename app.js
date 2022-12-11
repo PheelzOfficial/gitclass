50b8f3a0dc0af45330a52c3258bddcd1d6d5a5c7
Popoola Philip <popoolaphilip10@gmail.com>
2022-12-11 16:10:10 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+
